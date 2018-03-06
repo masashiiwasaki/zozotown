d2e080543f9b60cc266c0b2fe05bd2e2ed6f513c
Your Name <you@example.com>
2018-03-06 17:55:23 +0900

    Created Color model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180306084512) do
+ActiveRecord::Schema.define(version: 20180306085223) do
 
   create_table "carts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id",    null: false
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 20180306084512) do
     t.index ["item_id"], name: "index_carts_on_item_id", using: :btree
     t.index ["size_id"], name: "index_carts_on_size_id", using: :btree
     t.index ["user_id"], name: "index_carts_on_user_id", using: :btree
+  end
+
+  create_table "colors", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "color"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "item_lists", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
