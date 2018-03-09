84607df486feb2d104c72b3121722336b440b85b
Your Name <you@example.com>
2018-03-09 17:31:12 +0900

    Modify variables

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,11 +4,11 @@ class Cart < ApplicationRecord
   belongs_to :color
   belongs_to :size
 
-    def self.total_price(carts)
-    @total_price = 0
+  def total_price(carts)
+    total_price = 0
     carts.each do |cart|
-      @total_price += cart.item.price
+      total_price += cart.item.price
     end
-    return @total_price
+    return total_price
   end
 end
