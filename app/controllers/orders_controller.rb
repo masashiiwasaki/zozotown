5d53ef81b5c5dbcf4c5d1ffdad8302c6aff82186
Jun Miyakawa <fcfym630@gmail.com>
2018-03-09 09:31:43 +0900

    コントローラに決済機能を実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,7 @@ class OrdersController < ApplicationController
   end
 
   def create
-    
+    Payjp.api_key = PAYJP_SECRET_KEY
+    Payjp::Charge.create(currency: 'jpy', amount: params[:amount], card: params['payjp-token'])
   end
 end
