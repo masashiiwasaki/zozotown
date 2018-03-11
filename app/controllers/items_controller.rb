4f722f8748deb42850c137d84468f417373a1159
Jun Miyakawa <fcfym630@gmail.com>
2018-03-11 20:14:35 +0900

    チェック済（詳細画面で閲覧した）商品が一覧画面に表示されるように改修

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,18 @@
 class ItemsController < ApplicationController
   def index
     @items = Item.includes(:shop).includes(:brand)
+    # チェック済商品を画面表示
+    @checked_items = Item.where(id: session[:checked_item_ids])
   end
   def show
     # チェックした商品をsessionに保存し、チェック済商品を画面表示できるようにする
     # sessionがない場合は初期化
-    unless session[:checked_items]
-      session[:checked_items] = []
+    unless session[:checked_item_ids]
+      session[:checked_item_ids] = []
     end
     # sessionにチェックした商品IDを保存
-    session[:checked_items] << params[:id]
-    @checked_items = session[:checked_items]
+    session[:checked_item_ids] << params[:id]
+    @checked_items = Item.where(id: session[:checked_item_ids])
     @item = Item.find(params[:id])
   end
 end
