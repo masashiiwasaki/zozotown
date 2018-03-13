a62370118d9d768be7fe393ec314e2e0007cd446
岩崎真士 <amehurisode@yahoo.co.jp>
2018-03-13 17:06:02 +0900

    add foreign_key

--- db/migrate/20180313055344_add_rate_to_items.rb
+++ db/migrate/20180313055344_add_rate_to_items.rb
@@ -5,8 +5,8 @@ class AddRateToItems < ActiveRecord::Migration[5.0]
     add_column :items,    :regular_price,   :integer,    null: false
     add_column :items,    :proposed_price,  :integer,    null: false
     add_column :items,    :point_get,       :integer,    null: false, default: 0
-    add_column :items,    :gender,          :references, null: false
-    add_column :items,    :category,        :references, null: false
+    add_column :items,    :gender,          :references, null: false, foreign_key: true
+    add_column :items,    :category,        :references, null: false, foreign_key: true
     add_column :items,    :material,        :string
     add_column :items,    :made_in,         :string
     add_column :items,    :shipping_fee,    :integer,    null: false, default: 200
