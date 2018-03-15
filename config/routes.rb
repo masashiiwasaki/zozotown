e0d503463a10145f8dab2afab8d1baf445247b06
Your Name <you@example.com>
2018-03-15 11:25:03 +0900

    Define rootes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
+  resources :users only: [:show, :edit, :update, :destroy] do
+    resources :address_lists [:new, :create, :edit, :update, :destroy]
+  end
   resources :items, only: [:index, :show]
   resources :carts, only: [:index, :create, :update, :destroy]
   resources :orders, only: [:index, :new, :create]
