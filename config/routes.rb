27a365c1aac61ff8ba114b2fa33b94996b46fed8
Your Name <you@example.com>
2018-03-15 11:30:00 +0900

    Fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :users only: [:show, :edit, :update, :destroy] do
-    resources :address_lists [:new, :create, :edit, :update, :destroy]
+  resources :users, only: [:show, :edit, :update, :destroy] do
+    resources :address_lists, only: [:new, :create, :edit, :update, :destroy]
   end
   resources :items, only: [:index, :show]
   resources :carts, only: [:index, :create, :update, :destroy]
