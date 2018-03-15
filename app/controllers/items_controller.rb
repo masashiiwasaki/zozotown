b58b8812ac6fa9af78e2a00f4e878f4de21bcb20
Jun Miyakawa <fcfym630@gmail.com>
2018-03-15 17:42:06 +0900

    複数検索条件で動的に検索できるようロジック改修

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,27 +25,33 @@ class ItemsController < ApplicationController
     # 検索パラメータによらないSQLの共通部分
     sql = "select i.* from items i inner join brands b on i.brand_id = b.id inner join shops s on i.shop_id = s.id where 1 = 1 "
     # 検索パラメータの有無に応じて動的にSQL組み換え
-    params_array = []
+    # バインドパラメータ（hash形式）も同時に動的作成
+    bind_params_hash = {}
     if search_param_keyword.present?
-      sql << "and i.name like :search_param_keyword"
-      params_array << {search_param_keyword: '%' +search_param_keyword + '%'}
+      sql << "and i.name like :search_param_keyword "
+      bind_params_hash[:search_param_keyword] = '%' +search_param_keyword + '%'
     end
-    # バインドパラメータ（hash形式）動的作成
-    sanitize_sql_array = []
-    sanitize_sql_array << sql
-    params_array.each do |params_hash|
-      sanitize_sql_array << params_hash
+    if search_param_brand.present?
+      sql << "and b.name like :search_param_brand "
+      bind_params_hash[:search_param_brand] = '%' +search_param_brand + '%'
+    end
+    if search_param_shop.present?
+      sql << "and s.name like :search_param_shop "
+      bind_params_hash[:search_param_shop] = '%' +search_param_shop + '%'
     end
     # SQL生成
-    sql = ActiveRecord::Base.send(
+    sanitize_sql_array = []
+    sanitize_sql_array << sql
+    sanitize_sql_array << bind_params_hash
+    exe_sql = ActiveRecord::Base.send(
       :sanitize_sql_array,
       sanitize_sql_array
     )
     # SQL実行
     con = ActiveRecord::Base.connection
-    result = con.select_all(sql)
+    result = con.select_all(exe_sql)
     @result_items = result.to_hash
     # 暫定的にviewを表示
-    render plain: "detail_search_result"
+    render plain: @result_items
   end
 end
