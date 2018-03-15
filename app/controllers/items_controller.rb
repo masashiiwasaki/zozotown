cd9a6ade851994cdda895ef83da8819dd4ee80c6
yuka-matsuura <appleidmtk@outlook.jp>
2018-03-15 19:30:17 +0900

    Add incremental search
    # What
    インクリメンタルサーチの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,27 @@ class ItemsController < ApplicationController
   def index
     @items = Item.includes(:shop).includes(:brand)
   end
+
   def show
     @item = Item.new
   end
+
+  def search
+    @searchItems = Item.where('name LIKE(?)', "%#{params[:keyword]}%")
+    # binding.pry
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
+  def searchResult
+    @searchItemsResult = Item.where(id: params[:id])
+    # binding.pry
+    respond_to do |format|
+      format.html
+      format.json {render json: @searchItemsResult }
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :items, only: [:index, :show]
+  resources :items, only: [:index, :show] do
+    collection do
+      get 'search'
+      get 'searchResult'
+    end
+  end
   resources :carts, only: [:index, :create, :update, :destroy]
   resources :orders, only: [:index, :new, :create]
 end
