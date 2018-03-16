2db54791d591498289b8c8227fa70c34795980c9
岩崎真士 <amehurisode@yahoo.co.jp>
2018-03-16 15:31:21 +0900

    ルーテイング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :items, only: [:index, :show] do
-  resources :users, only: [:show, :edit, :update, :destroy] do
-    resources :address_lists, only: [:new, :create, :edit, :update, :destroy]
-  end
   get '/items/detail_search', to: 'items#detail_search', as: 'detail_search_items'
   get '/items/detail_search_result', to: 'items#detail_search_result', as: 'detail_search_result_items'
-  resources :items, only: [:index, :show] do  
+  resources :items, only: [:index, :show] do
     resources :favorite_item, only: [:create, :destroy] do
       collection do
         get 'search'
