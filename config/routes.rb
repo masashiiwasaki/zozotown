85b9549404fd5216eaed5c8c3d5cd3edbb2d9cff
yuka-matsuura <appleidmtk@outlook.jp>
2018-03-17 10:45:15 +0900

    Fix incremental Search routing
    # What
    インクリメンタルサーチが動作するようルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,11 @@ Rails.application.routes.draw do
   get '/items/detail_search', to: 'items#detail_search', as: 'detail_search_items'
   get '/items/detail_search_result', to: 'items#detail_search_result', as: 'detail_search_result_items'
   resources :items, only: [:index, :show] do
-    resources :favorite_item, only: [:create, :destroy] do
-      collection do
-        get 'search'
-        get 'searchResult'
-      end
+    collection do
+      get 'search'
+      get 'searchResult'
     end
+    resources :favorite_item, only: [:create, :destroy]
   end
   resources :carts, only: [:index, :create, :update, :destroy]
   resources :orders, only: [:index, :new, :create]
